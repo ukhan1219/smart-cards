427d83cd010ecb290aab4a3672f722d5f0799188
Maverick Reynolds <mcreynolds02@gmail.com>
2024-09-28 03:34:04 -0400

    rest

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './components/Home';
 import Upload from './components/Upload';
+import Items from './components/Items';
+import Insights from './components/Insights'; 
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route exact path="/" Component={Home} />
         <Route exact path="/upload" Component={Upload} />
+        <Route exact path="/items" Component={Items} />
+        <Route exact path="/insights" Component={Insights} />
       </Routes>
     </BrowserRouter>
   );
