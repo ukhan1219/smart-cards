7dd971b9d34b6a8cf68b8e60740b6d72af7ae97c
Maverick Reynolds <mcreynolds02@gmail.com>
2024-09-28 03:26:44 -0400

    routing

---
+++ src/components/Upload.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Upload = () => {
+  return (
+    <div>
+      <h1>Upload Page</h1>
+      <p>This is the Upload Page!</p>
+    </div>
+  );
+};
+
+export default Upload;
