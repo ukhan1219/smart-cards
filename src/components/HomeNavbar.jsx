271afe0e171f67b1d79c7456c0b593588ef240e4
DarkShadowNova <sri567890@gmail.com>
2024-09-28 14:45:02 -0400

    Added OAuth2 to Login page

--- src/components/HomeNavbar.jsx
+++ src/components/HomeNavbar.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logoSmartCards.png';
 
-function HomeNavbar() {
+const HomeNavbar = () => {
+  const handleLogin = () => {
+    window.location.href = 'http://localhost:5000/auth/google'; 
+  }; 
+
   return (
     <nav className="bg-blue-700 w-full">
       <div className="container mx-auto px-4">
@@ -12,15 +16,15 @@ function HomeNavbar() {
             <Link to="/" className="text-3xl font-bold text-white hover:text-blue-200">SmartCards</Link>
           </div>
           <div className="flex items-center space-x-4">
-            <Link to="/upload" className="text-white hover:text-blue-200 text-lg font-bold">About</Link>
-            <button className="bg-white text-blue-700 px-4 py-2 rounded hover:bg-blue-100 transition duration-150 ease-in-out">
-              <Link to="/upload" className="text-blue-700 hover:text-blue-900 text-lg font-bold">Log In</Link>
+            <Link to="/upload" className="text-white hover:text-blue-200 text-lg">About</Link>
+            <button onClick={handleLogin} className="bg-white text-blue-700 px-4 py-2 rounded hover:bg-blue-100 transition duration-150 ease-in-out">
+              <h1 className="text-blue-700 hover:text-blue-900 text-lg">Log In</h1>
             </button>
           </div>
         </div>
       </div>
     </nav>
   );
-}
+};
 
 export default HomeNavbar;
